a1871f4fac5096343dc69b67b27d3cec3715040a
siyapra <sr.prajapati.2050@gmail.com>
2025-03-27 17:13:56 +0530

    small changes

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -6,28 +6,31 @@ import Fitlift from '../assets/fitlift.png'
 const ProjectCard = () => {
     return (
         <>
+            <a href="https://the-fitness-collage.netlify.app/" target='_blank'>
+                <img className='hovers' src={Fitlift} alt="Viberr logo" />
+                <h3>FitLift</h3>
+                <p>Fitness App</p>
+            </a>
 
+            <a href="https://hippster.netlify.app/" target='_blank'>
+                <img className='hovers' src={hipsster} alt="Viberr logo" />
+                <h3>Hipsster</h3>
+                <p>Glasses Shop</p>
+            </a>
+            <a href="https://karisitascollage.netlify.app/" target='_blank'>
+                <img className='hovers' src={burger} alt="Viberr logo" />
+                <h3>Fresh Burger</h3>
+                <p>Hamburger Restaurant</p>
+            </a>
             <a href="https://tirp-planner.onrender.com/" target='_blank'>
                 <img className='hovers' src={Viberr} alt="Viberr logo" />
                 <h3>Viberr</h3>
                 <p>Streaming App</p>
             </a>
 
-            <a href="https://karisitascollage.netlify.app/" target='_blank'>
-                <img className='hovers' src={burger} alt="Viberr logo" />
-                <h3>Fresh Burger</h3>
-                <p>Hamburger Restaurant</p>
-            </a>
-            <a href="https://hippster.netlify.app/" target='_blank'>
-                <img className='hovers' src={hipsster} alt="Viberr logo" />
-                <h3>Hipsster</h3>
-                <p>Glasses Shop</p>
-            </a>
-            <a href="https://the-fitness-collage.netlify.app/" target='_blank'>
-                <img className='hovers' src={Fitlift} alt="Viberr logo" />
-                <h3>FitLift</h3>
-                <p>Fitness App</p>
-            </a>
+
+
+
         </>
     )
 }
